2a92a0135d8e623ee6a351f35af55e4899143603
Chris <ckopcow@gmail.com>
2021-05-02 21:17:05 -0400

    Added reminder to portrait parser

--- utils/portraitNameParser.ts
+++ utils/portraitNameParser.ts
@@ -4,11 +4,16 @@ const path = require('path');
 const portraitNameParser = (req, res, next) => {
   const items = readdirSync(path.join(__dirname, req.body.dir));
 
+  /**
+   * TODO: Check to see if the current element is a directory or a file
+   * to determine if we should parse the file name to create the correct
+   * drop-down menu or if we need to go another level deeper
+   */
   items.forEach((el) => {
     if (el.isDirectory()) {
-      portraitNameParser(req, res, next)
+
     } else if (el.isFile()) {
-      
+
     }
   })
 
