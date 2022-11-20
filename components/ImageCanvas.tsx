7c769e22a62daa93c23917eacbff9e2987d50dd7
Chris <ckopcow@gmail.com>
2022-11-20 15:10:40 -0500

    Finally! Added different sizes of blank dialogue boxes, finished custom name functionality

--- components/ImageCanvas.tsx
+++ components/ImageCanvas.tsx
@@ -14,22 +14,27 @@ const ImageCanvas = ({ portrait, name, text, font, char, emote, costume, box, se
   const character: React.MutableRefObject<any> = useRef(null);
   const dialogueBox: React.MutableRefObject<any> = useRef(null);
   let random: number;
+  let secondRandom: number;
+  let thirdRandom: number;
+  let textX: number = 418;
+  let textY: number = 438;
   let pCtx: CanvasRenderingContext2D;
   let bCtx: CanvasRenderingContext2D;
   let tileCtx: CanvasRenderingContext2D;
   let nCtx: CanvasRenderingContext2D;
   let tCtx: CanvasRenderingContext2D;
   let textObj: any; // "any" gets around a TS bug that says a fontBoundingBoxAscent property doesn't exist for a TextMetrics obj
+  let nameObj: any;
 
   useEffect(() => {
     // Initialize tile canvas
     tileCtx = tileCanvas.current.getContext('2d');
-    tileCtx.rotate(-14.75 * Math.PI / 180);
+    tileCtx.rotate(-14.65 * Math.PI / 180);
 
     // Initialize name canvas
     nCtx = nameCanvas.current.getContext('2d');
     nCtx.textAlign = 'left';
-    nCtx.rotate(-14.75 * Math.PI / 180);
+    nCtx.rotate(-14.65 * Math.PI / 180);
   }, []) // Empty dependency means it only runs on first render
 
   useEffect(() => {
@@ -43,7 +48,7 @@ const ImageCanvas = ({ portrait, name, text, font, char, emote, costume, box, se
     tileCtx.clearRect(0, 0, 1275, 500);
 
     // Measure entire name
-    const nameObj = nCtx.measureText(name);
+    nameObj = nCtx.measureText(name);
 
     // Check to see if current name has a pre-exisiting box
     let hasBox: string | null = findNameBox(name);
@@ -52,9 +57,10 @@ const ImageCanvas = ({ portrait, name, text, font, char, emote, costume, box, se
       setBox(`./images/boxes/db-${hasBox}-${font}.png`)
       return;
     } else {
-      // Otherwise use the blank box and draw the name
-      // TODO: Check the size of name and adjust size of box if necessary
-      setBox('./images/db@2x.png');
+      // If not, draw a blank box depending on the size of the name.
+      if (nameObj.width <= 195) { setBox('./images/db-small.png'); textX = 418; }
+      else if (nameObj.width > 195 && nameObj.width <= 275) { setBox('./images/db-medium.png'); textX = 456; }
+      else { setBox('./images/db-large.png'); textX = 495; }
     }
     
     // Edge case: If name is just whitespace, we won't enter the loop so it doesn't run infinitely
@@ -64,20 +70,78 @@ const ImageCanvas = ({ portrait, name, text, font, char, emote, costume, box, se
       do {
         random = Math.floor(Math.random() * name.length);
       } while (name[random] === ' ');
+
+
+      if (name.length >= 8) {
+        // Pick a new random number to make sure it's on the left side of the name
+        // That way we can ensure there's a nice balance to where the boxes are
+        // and make sure the first random number comes before the second random number
+        if (random > ((name.length / 2) - 1)) {
+          while (name[random] === ' ' || random > ((name.length /2) - 1)) {
+            random = Math.floor(Math.random() * name.length);
+          }
+        }
+
+        // Likewise, ensure the second random number is on the right half of the name
+        // and at least a few numbers away from the first random number
+        do {
+          secondRandom = Math.floor(Math.random() * name.length);
+        } while (name[secondRandom] === ' ' || secondRandom < ((name.length / 2) - 1) || Math.abs(secondRandom - random) <= 2);
+      }
+
+      if (name.length >= 16) {
+        if (random > (Math.floor((name.length / 3) - 1))) {
+          while (name[random] === ' ' || random > (Math.floor((name.length / 3) - 1))) {
+            random = Math.floor(Math.random() * name.length);
+          }
+        }
+        
+        if (secondRandom > (2 * (Math.floor((name.length / 3) - 1)))) {
+          while (name[secondRandom] === ' ' || secondRandom < (Math.floor((name.length / 3) - 1)) || secondRandom > (2 * (Math.floor((name.length / 3) - 1))) || Math.abs(secondRandom - random) <= 2) {
+            secondRandom = Math.floor(Math.random() * name.length);
+          }
+        }
+
+        do {
+          thirdRandom = Math.floor(Math.random() * name.length);
+        } while (name[thirdRandom] === ' ' || thirdRandom < (2 * (Math.floor((name.length / 3) - 1))) || Math.abs(thirdRandom - secondRandom) <= 2);
+      }
     } else { 
       random = null; 
     }
 
     // Split name into multiple substrings so we can change
     // the color of the character over the black tile
-      let beforeBox: string = name.substring(0, random);
-      let behindBox: string = name.substring(random, random + 1);
-      let afterBox: string = name.substring(random + 1);
+    let beforeBox: string = name.substring(0, random);
+    let behindBox: string = name.substring(random, random + 1);
+    let afterBox: string = name.substring(random + 1);
+    
+    let secondBehindBox: string;
+    let secondAfterBox: string;
+    let thirdBehindBox: string;
+    let thirdAfterBox: string;
+
+    if (name.length >= 8) {
+      afterBox = name.substring(random + 1, secondRandom);
+      secondBehindBox = name.substring(secondRandom, secondRandom + 1);
+      secondAfterBox = name.substring(secondRandom + 1);
+
+      if (name.length >= 16) {
+        secondAfterBox = name.substring(secondRandom + 1, thirdRandom);
+        thirdBehindBox = name.substring(thirdRandom, thirdRandom + 1);
+        thirdAfterBox = name.substring(thirdRandom + 1);
+      }
+    }
+
+    console.log(beforeBox, behindBox, afterBox, secondBehindBox, secondAfterBox, thirdBehindBox, thirdAfterBox);
+    console.log(random, secondRandom, thirdRandom);
     
     loadedFont.load().then(() => {
 
       // Find the leftmost coordinate of the name on the canvas 
-      let boxDrawX = 418 - nameObj.width / 2; // TODO: change 418 to a variable that represents the center of each dialogue box name area
+      let boxX: number = textX - nameObj.width / 2; // TODO: change 418 to a variable that represents the center of each dialogue box name area
+      let secondBoxX: number;
+      let thirdBoxX: number;
 
       // Edge case: No black boxes for single character names or whitespace-only names
       if (name.length > 1 && name.trim()) {
@@ -85,16 +149,54 @@ const ImageCanvas = ({ portrait, name, text, font, char, emote, costume, box, se
         // start drawing the box behind the randomly chosen character
         for (let i = 0; i < random; i++) {
           textObj = nCtx.measureText(name[i]);
-          boxDrawX += textObj.width;
+          boxX += textObj.width;
         };
 
         textObj = nCtx.measureText(name[random]);
+
         tileCtx.fillRect(
-          boxDrawX,
-          438 - textObj.fontBoundingBoxAscent,
+          boxX,
+          438 - textObj.fontBoundingBoxAscent - 4,
           textObj.width,
-          textObj.fontBoundingBoxAscent + textObj.fontBoundingBoxDescent
+          textObj.fontBoundingBoxAscent + textObj.fontBoundingBoxDescent + 7
         );
+
+        if (name.length >= 8) {
+          secondBoxX = boxX;
+
+          for (let i = random; i < secondRandom; i++) {
+            textObj = nCtx.measureText(name[i]);
+            secondBoxX += textObj.width;
+          };
+
+          textObj = nCtx.measureText(name[secondRandom]);
+
+          tileCtx.fillRect(
+            secondBoxX,
+            438 - textObj.fontBoundingBoxAscent - 3,
+            textObj.width,
+            textObj.fontBoundingBoxAscent + textObj.fontBoundingBoxDescent + 7
+          );
+        }
+
+        if (name.length >= 16) {
+          thirdBoxX = secondBoxX;
+
+          for (let i = secondRandom; i < thirdRandom; i++) {
+            textObj = nCtx.measureText(name[i]);
+            thirdBoxX += textObj.width;
+          };
+
+          textObj = nCtx.measureText(name[thirdRandom]);
+
+          tileCtx.fillRect(
+            thirdBoxX,
+            438 - textObj.fontBoundingBoxAscent - 3,
+            textObj.width,
+            textObj.fontBoundingBoxAscent + textObj.fontBoundingBoxDescent + 6
+          );
+        }
+
       // Edge case: Draw entire name, if one character
       } else if (name.length === 1) { 
         nCtx.fillStyle = '#000000';
@@ -103,13 +205,29 @@ const ImageCanvas = ({ portrait, name, text, font, char, emote, costume, box, se
       }
       
       nCtx.fillStyle = '#000000';
-      nCtx.fillText(beforeBox, 418 - nameObj.width / 2, 438); // TODO: change 438 to a variable that represents the Y coordinate of each dialogue box name area
+      nCtx.fillText(beforeBox, textX - nameObj.width / 2, textY); // TODO: change 438 to a variable that represents the Y coordinate of each dialogue box name area
       nCtx.fillStyle = '#FFFFFF'; // White text appears on black tile
       textObj = nCtx.measureText(beforeBox);
-      nCtx.fillText(behindBox, 418 - (nameObj.width / 2) + textObj.width, 438);
+      nCtx.fillText(behindBox, boxX, textY);
       nCtx.fillStyle = '#000000';
       textObj = nCtx.measureText(name[random]); 
-      nCtx.fillText(afterBox, boxDrawX + textObj.width, 438);
+      nCtx.fillText(afterBox, boxX + textObj.width, textY);
+
+      if (name.length >= 8) {
+        nCtx.fillStyle = '#FFFFFF';
+        nCtx.fillText(secondBehindBox, secondBoxX, textY);
+        nCtx.fillStyle = '#000000';
+        textObj = nCtx.measureText(name[secondRandom]); 
+        nCtx.fillText(secondAfterBox, secondBoxX + textObj.width, textY);
+
+        if (name.length >= 16) {
+          nCtx.fillStyle = '#FFFFFF';
+          nCtx.fillText(thirdBehindBox, thirdBoxX, textY);
+          nCtx.fillStyle = '#000000';
+          textObj = nCtx.measureText(name[thirdRandom]); 
+          nCtx.fillText(thirdAfterBox, thirdBoxX + textObj.width, textY);
+        }
+      }
 
       return;
     });
